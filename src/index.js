d673fab2a5dab7fc3275529f3de56f8e36e313eb
stephanie <stephieplusthree@gmail.com>
2020-11-04 19:59:54 -0800

    store added

--- src/index.js
+++ src/index.js
@@ -1,25 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+} from "react-router-dom";
+import { Provider, connect } from "react-redux";
+import { createStore } from "redux";
 import Register from "./components/Auth/Register/Register.component";
 import Login from "./components/Auth/Login/Login.component";
+import firebase from "./server/firebase";
+import { combinedReducers } from "./store/reducer";
+import { setUser } from "./store/actioncreator";
 
 import "semantic-ui-css/semantic.min.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
+const store = createStore(combinedReducers);
+
+const Index = (props) => {
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        props.setUser(user);
+        props.history.push("/");
+      } else {
+        props.setUser(null);
+        props.history.push("/login");
+      }
+    });
+  }, []);
+
+  console.log("Debug", props.currentUser);
+
+  return (
     <Switch>
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
       <Route path="/" component={App} />
     </Switch>
-    </Router>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.user.currentUser,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setUser: (user) => {
+      dispatch(setUser(user));
+    },
+  };
+};
+
+const IndexWithRouter = withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(Index)
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <IndexWithRouter />
+      </Router>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
