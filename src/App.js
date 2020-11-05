9e2557037a25299da67182e0b10987ac6cee83f7
stephanie <stephieplusthree@gmail.com>
2020-11-04 21:08:12 -0800

    sidebar component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import './App.css';
+import { SideBar } from "./components/Sidebar/SideBar.component";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <SideBar />
     </div>
   );
 }
---
+++ src/components/Sidebar/SideBar.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Menu} from "semantic-ui-react";
+
+import "./SideBar.css";
+
+export const SideBar = () => {
+  return ( <Menu vertical fixed="left" borderless size="large"  className="side_bar">
+
+  </Menu>  
+  )
+}
